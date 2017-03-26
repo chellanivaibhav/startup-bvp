c17ba713730d62f188ecf1de54cb4a8905330c06
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-03-26 11:43:40 +0530

    implementing search part

--- search.php
+++ search.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+</body>
+<!-- connects to database  WHERE gender=$_POST["Gender"]-->
+<?php require 'connToDB.php';?>
+<br>
 <?php
+$Gender=lcfirst($_POST["Gender"]);
+$sql=$conn->prepare("SELECT * FROM property_listings WHERE 
+	gender = '$Gender'");
+$sql->execute();
+
+while($row=$sql->fetch(PDO::FETCH_ASSOC)){	
+	echo ($row['property_id'] . $row['rent_pm'] . $row['gender'].$row['Distance_College']);
+	echo nl2br("\n");
+}
+
+
+?>
+
+
+</body>
+</html>
